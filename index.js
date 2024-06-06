bc11af65dd98753358632b8accbf8ebd07ba4302
mariana-avila-rivera <marianaavilarivera@gmail.com>
2024-06-06 02:30:33 -0600

    editando cors con url de railway

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const misrutas = require("./routes/rutas"); //importar rutas
 
 const app = express(); //crear al servidor
 const port = process.env.PORT || 3000;
-app.use(cors());
+// Configura CORS para permitir solicitudes desde tu frontend
+app.use(
+  cors({
+    origin: "https://prueba-production-8f21.up.railway.app", // Reemplaza con el dominio de tu frontend
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,5 +21,5 @@ app.use(express.static(process.cwd() + "/public/")); //middleware para cargar ar
 //El middleware es una función que se ejecuta antes de que se ejecute la función de la ruta.
 
 app.listen(port, () => {
-  console.log(`hola servidor ejecucion en http://localhost:${port}`);
+  console.log(`hola servidor ejecucion en puerto: ${port}`);
 });
